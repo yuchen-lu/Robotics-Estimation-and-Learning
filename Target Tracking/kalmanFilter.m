5014c5eaa65649667eb0bf57bf7e594bfc9d4b24
yuchen-lu <30705243+yuchen-lu@users.noreply.github.com>
2017-08-29 20:31:02 -0400

    Add files via upload

--- Target Tracking/kalmanFilter.m
+++ Target Tracking/kalmanFilter.m
@@ -8,7 +8,6 @@ function [ predictx, predicty, state, param ] = kalmanFilter( t, x, y, state, pa
     dt=0.033;
     A=[1 0 dt 0; 0 1 0 dt ; 0 0 1 0; 0 0 0 1];
     C=[1 0 0 0; 0 1 0 0];
-    
     % Check if the first time running this function
     if previous_t<0
         state = [x, y, 0, 0];
@@ -21,12 +20,22 @@ function [ predictx, predicty, state, param ] = kalmanFilter( t, x, y, state, pa
     %% TODO: Add Kalman filter updates
     % As an example, here is a Naive estimate without a Kalman filter
     % You should replace this code
-    norm=normpdf(state,[0;0;0;0],)
-    vx = A(4,4)*state(4) + normpdf(, ,)
-    vy = (y - state(2)) / (t - previous_t);
+    covarm = diag([0.01 0.01 0.1 0.1]);
+    covaro = diag([0.01 0.01]);
+    
+    N1=normpdf(state, [0;0;0;0],covarm);
+    Nvx=N1(3,3);
+    Nvy=N1(4,4);
+    
+    vx = state(3) + Nvx;
+    vy = state(4) + Nvy;
     % Predict 330ms into the future
-    predictx = x + vx * 0.330;
-    predicty = y + vy * 0.330;
-    % State is a four dimensional element
+    
+    N2=normpdf([state(1);state(2)], [0;0],covaro);
+    Nx=N2(1,1);
+    Ny=N2(2,2);
+    predictx = x + Nx;
+    predicty = y + Ny;
+    % State is a  four dimensional element
     state = [x, y, vx, vy];
 end
---
+++ Target Tracking/runeval.m
@@ -0,0 +1,2 @@
+testpath = '/u/b/luyuche1/Desktop/files-w2'; % This is the folder where test.mat is saved. Change the path if needed.
+eval_progW2(testpath)
